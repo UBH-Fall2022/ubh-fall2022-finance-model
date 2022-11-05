2ff3e68f48d5d82a0ae0f1c0a040ba3b7332eb9f
JimmyWu7 <104340474+JimmyWu7@users.noreply.github.com>
2022-11-05 16:12:40 -0400

    Line Graph changes

--- processing.py
+++ processing.py
@@ -1,31 +1,3 @@
-def max_value(data,y): 
-  word=""
-  for dict in data:
-    for key in dict:
-      if key==y:
-        if dict[key]>word:
-          word=dict[key]
-  return word
-
-def init_dictionary(data,y):
-  out={}
-  v=0
-  for dict in data:
-    for key in dict:
-      if key==y:
-        if dict[key]==dict[y]:
-          out[dict[key]]=v
-  return out
-
-def sum_matches(lod,k,v,tgt):
-  out=0
-  for dict in lod:
-    for key in dict:
-      if key==k:
-        if dict[key]==v:
-          out+=float(dict[tgt])
-  return out
-
 def portions(num):
     return [num * 0.5,num * 0.3,num * 0.2]
 
@@ -34,13 +6,3 @@ def annualAmount(an,sav):
     for i in range(0,11):
         data.append(((an * 0.2) * i)+sav)
     return data
-
-
-def copy_matching(lod,k,v):
-  out=[]
-  for dict in lod:
-    for key in dict:
-      if key==k:
-        if dict[key]==v:
-          out.append(dict)
-  return out
