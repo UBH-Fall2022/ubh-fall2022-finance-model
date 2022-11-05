6a4206b47a3ca4cba6efdf10681f50050c0489da
elphan <starsoccerethan@gmail.com>
2022-11-05 17:28:34 -0400

    fixed more frontend! big pain :(

--- frontend.js
+++ frontend.js
@@ -5,26 +5,24 @@ function sendPieGraph(jsonBlob){
       height: 400,
       width: 500
     };
-  Plotly.newPlot('piegraph',loadData,layout)
+    Plotly.newPlot('pie',loadData,layout)
 }
 function sendLineGraph(jsonBlob) {
-    let location=document.getElementById("locText").value
-    let loadData=JSON.parse(jsonBlob)
+    let loadData = JSON.parse(jsonBlob)
     let layout = {
         title: 'Annual Financial Savings Graph',
         xaxis: {title: "Years"},
         yaxis: {title: "Amount of Money"},
-        type:'scatter'
+        type:'line'
     };
-    Plotly.newPlot('linegraph', loadData, layout);
+    Plotly.newPlot('line', loadData, layout);
 }
-  
-  function getGraphs(){
+function getGraphs(){
     let id=document.getElementById("annual");
     let id2=document.getElementById("sc");
     let string=JSON.stringify([id["value"],id2["value"]]);
+    let id3 = document.getElementById("weekly");
+    let string3 =JSON.stringify(id3["value"]);
     ajaxPostRequest('/linegraph', string, sendLineGraph);
-    id = document.getElementById("weekly");
-    string=JSON.stringify(id["value"]);
-    ajaxPostRequest('/piegraph', string, sendPieGraph);
-  }
+    ajaxPostRequest('/piegraph', string3, sendPieGraph);
+}
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import bottle
 import processing
 import json
 
+#routing static files
 @bottle.route("/")
 def index():
   return bottle.static_file("index.html",".")
@@ -20,34 +21,31 @@ def stylesheets(filename):
 @bottle.route('/images/<filename:re:.*\.png>')
 def send_image(filename):
     return bottle.static_file(filename, root='images/', mimetype='image/png')
-
 @bottle.route("/ajax.js")
 def javascript():
   return bottle.static_file("ajax.js",".")
-
-@bottle.route("/graphs.js")
+@bottle.route("/frontend.js")
 def graphJavascript():
-  return bottle.static_file("graphs.js",".")
-
+  return bottle.static_file("frontend.js",".")
+#serving graphs
 @bottle.post("/piegraph")
 def sendPieGraph():
     content=bottle.request.body.read().decode()
     textbox=json.loads(content)
-    data = processing.portions(textbox)
+    data = processing.portions(float(textbox))
     pie_values = [{
     "values": [data[0], data[1], data[2]],
     "labels": ['Needs', 'Wants', 'Savings'],
     "type": 'pie'
     }]
     return json.dumps(pie_values)
-
 @bottle.post("/linegraph")
 def sendLineGraph():
     content=bottle.request.body.read().decode()
     textbox=json.loads(content)
     xvalues= range(2022, 2033)
-    yvalues= processing.annualAmount(textbox[0],textbox[1])
-    line_values = [{"x":xvalues,"y":yvalues,"type":'scatter'}]
+    yvalues= processing.annualAmount(float(textbox[0]),float(textbox[1]))
+    line_values = [{"x":xvalues,"y":yvalues,"type":"line"}]
     return json.dumps(line_values)
 
 bottle.run(host = "0.0.0.0",port = 8080)
